65c3af0a7eca86874c3910f9a17accac67e80b5e
Naresh <ediganareshgoud111@gmail.com>
2026-06-03 16:38:34 +0530

    Initialized the backend for project

---
+++ Backend/src/main/java/com/doc/cloudecollaboration/CloudeCollaborationApplication.java
@@ -0,0 +1,13 @@
+package com.doc.cloudecollaboration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudeCollaborationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudeCollaborationApplication.class, args);
+    }
+
+}
---
+++ Backend/src/test/java/com/doc/cloudecollaboration/CloudeCollaborationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doc.cloudecollaboration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudeCollaborationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
